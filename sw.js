6a3d8a8fa3fe93a48bc24bded2ef48c80de145a5
Dixxxxxz <binffbeta06@gmail.com>
2026-08-02 19:35:35 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'celenganku-v1';
+const FILES = [
+  'index.html',
+  'manifest.json',
+  'icon-192.png',
+  'icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE).map(key => caches.delete(key))
+      );
+    })
+  );
+});
